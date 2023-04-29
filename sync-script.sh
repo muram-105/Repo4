976fe9346a36e7c326e9900a427fe4072206b43d
muram-105 <123832828+muram-105@users.noreply.github.com>
2023-04-29 16:01:54 +0300

    Update sync-script.sh

--- sync-script.sh
+++ sync-script.sh
@@ -6,7 +6,8 @@ git checkout main
 
 git remote add Repo4 https://github.com/muram-105/Repo4.git
 
-# git fetch Repo4 main
+git fetch Repo4 main
+git commit -m "add changes"
 
 # git merge Repo4/main
 
