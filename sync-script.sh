a7be56d1949038e767e528c6f241cbde7eebdefd
muram-105 <123832828+muram-105@users.noreply.github.com>
2023-04-29 16:25:39 +0300

    Update sync-script.sh

--- sync-script.sh
+++ sync-script.sh
@@ -7,14 +7,14 @@ git checkout main
 
 git config --global user.email "muram8700@gmail.com"
 git config --global user.name "muram-105"
-git remote add main https://github.com/muram-105/Repo4.git
+git remote add target https://github.com/muram-105/Repo4.git
 
 git fetch origin main
 
 # git commit -m "add changes"
 
 git merge origin/main
-git push Repo4 main
+git push target main
 
 # cd ..
 # rm -rf Repo3
