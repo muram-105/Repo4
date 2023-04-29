8ed4c0c77ff15c1ef91c1afcae2b2607b8e88f42
muram-105 <123832828+muram-105@users.noreply.github.com>
2023-04-29 15:31:09 +0300

    Update sync-script.sh

--- sync-script.sh
+++ sync-script.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Clone the source repository
 git clone https://github.com/muram-105/Repo3.git
-cd Repo3
+# cd Repo3
 
-# Check out the source branch
-git checkout source-branch
+git checkout Rep3
 
-# Add a remote for the target repository
 git remote add target-repo https://github.com/muram-105/Repo4.git
 
-# Fetch the latest changes from the target branch of the target repository
 git fetch Repo4 main
 
-# Merge the changes from the target branch of the target repository into the source branch
 git merge Repo4/main
 
-# Push the changes to the source branch of the source repository
-git push origin smain
+git push origin main
 
-# Clean up
 cd ..
 rm -rf Repo3
